28cf48f7a38b4d0ebf461bfb0d555696e5cff2cf
Chris Helmich <kagekirin@gmail.com>
2025-08-22 13:31:53 +0900

    feat: implement `XElement.GetBooleanAttribute()` extension methods

---
+++ ILRepack.Fody/Extensions/XElementExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using Fody;
+
+internal static class XElementExtensions
+{
+    public static bool ReadBoolAttribute(this XElement config, string nodeName, bool @default)
+    {
+        return config.ReadBoolAttribute(nodeName) ?? @default;
+    }
+
+    public static bool? ReadBoolAttribute(this XElement config, string nodeName)
+    {
+        var attribute = config.Attribute(nodeName);
+        if (attribute is not null)
+        {
+            try
+            {
+                return XmlConvert.ToBoolean(attribute.Value.ToLowerInvariant());
+            }
+            catch
+            {
+                throw new WeavingException(
+                    $"Could not parse '{nodeName}' from '{attribute.Value}'."
+                );
+            }
+        }
+
+        return null;
+    }
+}

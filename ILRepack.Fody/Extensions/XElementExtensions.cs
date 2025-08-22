3273473d8ebd0dc869d7bcacaff59c78a13a59aa
Chris Helmich <kagekirin@gmail.com>
2025-08-22 13:32:15 +0900

    feat: implement `XElement.ReadIntegerAttribute()` extension methods

--- ILRepack.Fody/Extensions/XElementExtensions.cs
+++ ILRepack.Fody/Extensions/XElementExtensions.cs
@@ -30,4 +30,29 @@ internal static class XElementExtensions
 
         return null;
     }
+
+    public static int ReadIntegerAttribute(this XElement config, string nodeName, int @default)
+    {
+        return config.ReadIntegerAttribute(nodeName) ?? @default;
+    }
+
+    public static int? ReadIntegerAttribute(this XElement config, string nodeName)
+    {
+        var attribute = config.Attribute(nodeName);
+        if (attribute is not null)
+        {
+            try
+            {
+                return XmlConvert.ToInt32(attribute.Value.ToLowerInvariant());
+            }
+            catch
+            {
+                throw new WeavingException(
+                    $"Could not parse '{nodeName}' from '{attribute.Value}'."
+                );
+            }
+        }
+
+        return null;
+    }
 }
